b7869b31877b879ffc368d09446291902dfee9d8
Andre <andre@alliancechemical.com>
2025-09-02 15:54:56 -0500

    Fix syntax error - remove extra closing braces

--- components/desktop/PrintPreparationModal.tsx
+++ components/desktop/PrintPreparationModal.tsx
@@ -276,7 +276,6 @@ export default function PrintPreparationModal({
                             )}
                           </div>
                         ))
-                      })}
                     </div>
                   </div>
                 )}
